32874b9a0a2baafe69dbe16b4bf3ffd5ab87b1c0
Jackson Doherty <jgdoherty23@gmail.com>
2017-06-13 12:01:40 -0500

    steam audio package added, 3d unity audio setup

---
+++ Assets/Phonon/Export/Editor/PhononMaterialDrawer.cs
@@ -0,0 +1,55 @@
+﻿//
+// Copyright (C) Valve Corporation. All rights reserved.
+//
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Phonon
+{
+
+    //
+    // PhononMaterialDrawer
+    // Custom property drawer for PhononMaterialValue.
+    //
+
+    [CustomPropertyDrawer(typeof(PhononMaterialValue))]
+    public class PhononMaterialDrawer : PropertyDrawer
+    {
+        //
+        // Returns the total height of the field.
+        //
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return 112f;
+        }
+
+        //
+        // Draws the field.
+        //
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            position.height = 16f;
+
+            if (position.x <= 0)
+            {
+                position.x += 4f;
+                position.width -= 8f;
+            }
+
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("LowFreqAbsorption"));
+            position.y += 16f;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("MidFreqAbsorption"));
+            position.y += 16f;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("HighFreqAbsorption"));
+            position.y += 16f;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("Scattering"));
+            position.y += 16f;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("LowFreqTransmission"));
+            position.y += 16f;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("MidFreqTransmission"));
+            position.y += 16f;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("HighFreqTransmission"));
+        }
+    }
+}
