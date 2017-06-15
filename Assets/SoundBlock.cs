965ba3f473fb23494f3cc7be50aa5fb75f260535
Jackson Doherty <jgdoherty23@gmail.com>
2017-06-14 22:03:10 -0500

    sound play on touch

---
+++ Assets/SoundBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SoundBlock : VRTK_InteractableObject {
+
+    private AudioSource sound;
+
+    protected override void Awake() {
+        base.Awake();
+
+        sound = GetComponent<AudioSource>();
+    }
+
+    public override void StartTouching(GameObject currentTouchingObject) {
+        base.StartTouching(currentTouchingObject);
+
+        if (currentTouchingObject.CompareTag("ControllerScript")) {
+            sound.Play();
+        }
+    }
+
+    public override void StopTouching(GameObject previousTouchingObject) {
+        base.StopTouching(previousTouchingObject);
+
+        if (previousTouchingObject.CompareTag("ControllerScript")) {
+            sound.Stop();
+        }
+    }
+}
