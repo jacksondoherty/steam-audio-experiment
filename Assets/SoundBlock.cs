7549e95e90627a3f9a10e8c580baa9fbb62398e5
Jackson Doherty <jgdoherty23@gmail.com>
2017-06-22 21:35:43 -0500

    volume adjuster

--- Assets/SoundBlock.cs
+++ Assets/SoundBlock.cs
@@ -6,11 +6,52 @@ using VRTK;
 public class SoundBlock : VRTK_InteractableObject {
 
     private AudioSource sound;
+    // todo: perhaps save script instead
+    private GameObject lastTouching;
+    private float initialScaleX; // x, y, z all equal
+    private float initialVolume;
 
     protected override void Awake() {
         base.Awake();
 
         sound = GetComponent<AudioSource>();
+        initialScaleX = transform.localScale.x;
+        initialVolume = sound.volume;
+    }
+
+    protected override void Update() {
+        base.Update();
+
+        // pedal
+        if (lastTouching) {
+            VRTK_ControllerEvents script = lastTouching.GetComponent<VRTK_ControllerEvents>();
+            if (!script.gripPressed) {
+                sound.Stop();
+                lastTouching = null;
+            }
+        }
+
+        // volume
+        float scaleDelta = transform.localScale.x / initialScaleX;
+        sound.volume = initialVolume * scaleDelta*scaleDelta;
+    }
+
+    // block hit
+    public override void StartTouching(GameObject currentTouchingObject) {
+        base.StartTouching(currentTouchingObject);
+
+        if (touchingObjects.Count == 1) {
+            sound.Play();
+        }
+    }
+
+    // block release
+    public override void StopTouching(GameObject previousTouchingObject) {
+        base.StopTouching(previousTouchingObject);
+        
+        if (touchingObjects.Count == 0) {
+            lastTouching = previousTouchingObject;
+        }
     }
 
     // overriding to not toggle if currently grabbed
@@ -23,22 +64,6 @@ public class SoundBlock : VRTK_InteractableObject {
             } else {
                 objectHighlighter.Unhighlight();
             }
-        }
-    }
-
-    public override void StartTouching(GameObject currentTouchingObject) {
-        base.StartTouching(currentTouchingObject);
-
-        if (touchingObjects.Count == 1) {
-            sound.Play();
-        }
-    }
-
-    public override void StopTouching(GameObject previousTouchingObject) {
-        base.StopTouching(previousTouchingObject);
-
-        if (touchingObjects.Count == 0) {   // can put pedal boolean here
-            sound.Stop();
         }
     }
 }
